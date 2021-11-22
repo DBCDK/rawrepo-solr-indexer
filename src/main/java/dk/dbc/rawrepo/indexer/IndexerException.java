26cb7620c993c602d084d42454ac979f113d2e24
Anders Thrane Michelsen <atm@dbc.dk>
2021-11-22 10:59:26 +0100

    Clean up
    Auditors: akri, mib, jbr, hwha

--- src/main/java/dk/dbc/rawrepo/indexer/IndexerException.java
+++ src/main/java/dk/dbc/rawrepo/indexer/IndexerException.java
@@ -2,9 +2,9 @@ package dk.dbc.rawrepo.indexer;
 
 public class IndexerException extends Exception {
 
-        /**
-    *
-    */
+    /**
+     *
+     */
     private static final long serialVersionUID = -7670152284752524062L;
 
     public IndexerException(String message, Throwable cause) {
--- src/main/resources/entrypoint.js
+++ src/main/resources/entrypoint.js
@@ -10,14 +10,12 @@ use( "Log" );
  * @param {String} content The collection of records in a MarcXChange collection encoded as a String
  * @param {Object} suppDataRaw JSON DTO encoded as a String which has supplementary data for the Javascript
  *        {String} mimetype The mimetype of the main record
- *        {Boolean} isHeadOrSectionHasChildren True if the main record is a head or section record and the record has children. Otherwise false
  *        {Integer} correctedAgencyId The "real" agency id of the main record
  * @return {String} The JSON document as a String to store in solr
  * @name index
  * @function
  */
 var index = function (content, suppDataRaw) {
-    Log.info( "Hejsa" );
     var suppData = JSON.parse(suppDataRaw);
     var xmlContent = XmlUtil.fromString( content );
 
